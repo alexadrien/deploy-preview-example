3139c21757a876ef408dac5d2af1e6525ff94983
alexadrien <alex.adrien.auger@gmail.com>
2019-03-06 22:55:40 +0100

    try this you cunt

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ssh -i ../devops/ssh/deploy-preview.pem ubuntu@ec2-34-244-153-202.eu-west-1.compute.amazonaws.com rm -rf /var/www/html/*
-scp -r -i ../devops/ssh/deploy-preview.pem ./build/* ubuntu@ec2-34-244-153-202.eu-west-1.compute.amazonaws.com:/var/www/html/
+scp -r -o StrictHostKeyChecking=no -i ../devops/ssh/deploy-preview.pem ./build/* ubuntu@ec2-34-244-153-202.eu-west-1.compute.amazonaws.com:/var/www/html/
