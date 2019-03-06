4e3abfd6c57649d4a6a5eac867376d53066b8c08
alexadrien <alex.adrien.auger@gmail.com>
2019-03-06 23:03:31 +0100

    oh la la

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 chmod 400 ../devops/ssh/deploy-preview.pem
-ssh -i -o StrictHostKeyChecking=no ../devops/ssh/deploy-preview.pem ubuntu@ec2-34-244-153-202.eu-west-1.compute.amazonaws.com rm -rf /var/www/html/*
+ssh -o StrictHostKeyChecking=no -i ../devops/ssh/deploy-preview.pem ubuntu@ec2-34-244-153-202.eu-west-1.compute.amazonaws.com rm -rf /var/www/html/*
 scp -r -o StrictHostKeyChecking=no -i ../devops/ssh/deploy-preview.pem ./build/* ubuntu@ec2-34-244-153-202.eu-west-1.compute.amazonaws.com:/var/www/html/
