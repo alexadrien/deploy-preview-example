f0c15a625e5d43f1391d33fc5df525b57d482ff5
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:51:16 +0100

    this

--- comment.sh
+++ comment.sh
@@ -2,7 +2,7 @@
 
 echo $CIRCLE_SHA1
 curl -X POST \
-  'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=819734796779a1ba79fc6552e21811dc02ee556a' \
+  'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN_COMMENTS' \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: 6a8282ec-8e8f-4c0c-8275-82687cb05797' \
   -H 'cache-control: no-cache' \
