d8d9201010c62c19cc29d0e90b7319c14cb4ebb3
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:17:47 +0100

    try like this

--- client/deploy_tmp.sh
+++ client/deploy_tmp.sh
@@ -3,4 +3,4 @@
 chmod 400 ../devops/ssh/deploy
 echo $TEMPORARY_EC2_URL
 ssh -o StrictHostKeyChecking=no -i ../devops/ssh/deploy ubuntu@$TEMPORARY_EC2_URL rm -rf /var/www/html/*
-scp -r -o StrictHostKeyChecking=no -i ../devops/ssh/deploy ./build/* ubuntu@$TEMPORARY_EC2_URL:/var/www/html/
+scp -r -o StrictHostKeyChecking=no -i ../devops/ssh/deploy /home/circleci/repo/client/build/* ubuntu@$TEMPORARY_EC2_URL:/var/www/html/
