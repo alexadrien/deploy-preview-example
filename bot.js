7a518a2d439e878e1ae276edaa832452928ebae2
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:39:21 +0100

    stuff comment

--- bot.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-const bot = require("circle-github-bot").create();
-
-bot.comment('<h3>${bot.env.commitMessage}</h3>');
---
+++ comment.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+curl -X POST \
+  'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$COMMIT_SHA1/comments?access_token=fc1d513e804fc7ac7b8e0946026c7f5406eaf6db' \
+  -H 'Content-Type: application/json' \
+  -H 'Postman-Token: 6a8282ec-8e8f-4c0c-8275-82687cb05797' \
+  -H 'cache-control: no-cache' \
+  -d '{
+  "body": "Great stuff"
+}
+'
