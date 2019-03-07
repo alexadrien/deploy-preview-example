567790b95af9af30eb862b0bc553a49a37a14081
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 19:38:02 +0100

    quote

--- devops/terraform/maint.tf
+++ devops/terraform/maint.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "main" {
   ami = "ami-00035f41c82244dab"
   instance_type = "t3.micro"
   key_name = "${aws_key_pair.deployer.key_name}"
-  security_groups = ['${aws_security_group.main}']
+  security_groups = ["${aws_security_group.main}"]
 }
 
 resource "aws_key_pair" "deployer" {
