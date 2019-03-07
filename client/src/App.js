1fb5469b7ea09433c2d060a317d466c11e1f13ea
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:39:48 +0100

    Learn sipios

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Sipios
           </a>
         </header>
       </div>
