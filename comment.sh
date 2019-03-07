87c48c1ec1721d5e7ce3ee4f24c5ef970213c67d
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:46:35 +0100

    try this

--- comment.sh
+++ comment.sh
@@ -2,7 +2,7 @@
 
 echo $CIRCLE_SHA1
 curl -X POST \
-  'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=fc1d513e804fc7ac7b8e0946026c7f5406eaf6db' \
+  'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=9ba722e292b6d4d470246c8c21d2cf1f953f5976' \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: 6a8282ec-8e8f-4c0c-8275-82687cb05797' \
   -H 'cache-control: no-cache' \
