84088faf8d4b20b26cd9bd848b8f74cade68549d
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:44:40 +0100

    echo

--- comment.sh
+++ comment.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo $CIRCLE_SHA1
 curl -X POST \
   'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=fc1d513e804fc7ac7b8e0946026c7f5406eaf6db' \
   -H 'Content-Type: application/json' \
