bb7f4a8f8bed0524f88e92c9b87947e7919fcf65
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:53:17 +0100

    try ""

--- comment.sh
+++ comment.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-echo 'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN_COMMENTS'
+echo "https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN_COMMENTS"
 curl -X POST \
-  'https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN_COMMENTS' \
+  "https://api.github.com/repos/alexadrien/deploy-preview-conf/commits/$CIRCLE_SHA1/comments?access_token=$GITHUB_TOKEN_COMMENTS" \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: 6a8282ec-8e8f-4c0c-8275-82687cb05797' \
   -H 'cache-control: no-cache' \
