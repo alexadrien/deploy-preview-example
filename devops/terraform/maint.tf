2619d4474178c717e5cac34cb9fd91fb56bfff82
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 19:49:13 +0100

    remove that

--- devops/terraform/maint.tf
+++ devops/terraform/maint.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "main" {
   instance_type = "t3.micro"
   key_name = "${aws_key_pair.deployer.key_name}"
   security_groups = "deploypreviewsg"
-  depends_on = ["aws_security_group.main"]
 }
 
 resource "aws_key_pair" "deployer" {
