bea0b2751ee08ddb84cf2d794a08f46d4735dde6
alexadrien <alex.adrien.auger@gmail.com>
2019-03-07 21:55:27 +0100

    with https

--- comment.sh
+++ comment.sh
@@ -7,6 +7,6 @@ curl -X POST \
   -H 'Postman-Token: 6a8282ec-8e8f-4c0c-8275-82687cb05797' \
   -H 'cache-control: no-cache' \
   -d "{
-  \"body\": \"$TEMPORARY_EC2_URL\"
+  \"body\": \"https://$TEMPORARY_EC2_URL\"
 }
 "
