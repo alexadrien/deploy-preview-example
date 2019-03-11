c426f46e5a1ae2ef1f5bc3b9c4b916cbd7dd5243
alexadrien <alex.adrien.auger@gmail.com>
2019-03-11 20:10:20 +0100

    this

--- devops/terraform/maint.tf
+++ devops/terraform/maint.tf
@@ -3,11 +3,18 @@ resource "aws_instance" "main" {
   instance_type = "t3.micro"
   key_name = "${aws_key_pair.deployer.key_name}"
   security_groups = ["deploypreviewsg"]
+
+  provisioner "local-exec" {
+    command = "sleep 10"
+  }
 }
 
 resource "aws_key_pair" "deployer" {
-//  key_name   = "deploy-preview-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIxzAeSe0n+AKiq7JIx2nQBw0mwtz4xgbrOiI1RzAL+Jd9eV7srpzk8zphj1cR7oNb2ZwXh55KGXiJh5Nq+iqKdJZs6Ppvsahol7+Sq7qr9clF0bZpuZ5epNonCf0hSCr8d8zLpQI6d4TOMS34XxTW/QqXCEOpHVjDJTWkGdAkKcMPy0tJJNMQVGZl2I/H7iJLM9Vz6LIADYZxzqhTFcbquCt+E0k2No8W48lKstTBW6qBvUGhWJdQbN0EjSwHDfHlxSmSvf3Jcqeyl8b4Do8sQzPWX512obhtDXPNEn76PYtirUMgfQ6V38yJ34BmzL6HGK/gicbMGNsRkVfLH00B acube@jensener.home"
+}
+
+output "public_dns" {
+  value = "${aws_instance.main.public_dns}"
 }
 
 variable "aws_access_key"   {}
@@ -18,8 +25,4 @@ provider "aws" {
   region = "eu-west-1"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-}
-
-output "public_dns" {
-  value = "${aws_instance.main.public_dns}"
 }
