5f43b0031c2df2490ff46082dcf0f3a1d791e5c9
Alex-Adrien Auger <alexadriena@sipios.com>
2019-03-11 20:01:24 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello React v2
+            Hello React
           </p>
           <a
             className="App-link"
